9b059bf23b1f588b9ac83e58b4703763d48881d2
Simon Grove <54975711+simo215d@users.noreply.github.com>
2020-09-17 11:56:03 +0200

    Update Main.java

--- src/client/Main.java
+++ src/client/Main.java
@@ -9,14 +9,29 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
     private static String fromServer;
+
+    //hackerman stuff
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
 
     //temperature graph fields
     private final NumberAxis tXAxis = new NumberAxis();
@@ -62,13 +77,16 @@ public class Main extends Application {
     private void startClient(){
         try {
             //vi skal bruger ip og port fra vores PI så vi kan begynde socket forbindelsen
-            Socket socket = new Socket("178.155.166.1", 8000);
+            Socket socket = new Socket("192.168.43.135", 8000);
             //vi bruger BufferedReader til at læse fra vores sockets inpurt stream som kommer fra serveren.
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             //hvert loop begynder med at vi venter på besked fra PI serveren. Når vi får en besked, så opdatere vi vores grafer.
             while (true){
                 fromServer = in.readLine();
                 System.out.println("received: " + fromServer);
+                String decrypted = decrypt(fromServer);
+                System.out.println("received decrypted: " + decrypted);
+                //System.out.println("decrypted: " + decrypt(fromServer));
                 //if server dies end program.
                 if (fromServer == null) {
                     System.out.println("Server DEAD");
@@ -79,10 +97,10 @@ public class Main extends Application {
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
-                        System.out.println("t: "+fromServer.substring(1,3)+"m: "+fromServer.substring(4));
+                        System.out.println("t: "+decrypted.substring(1,3)+"m: "+decrypted.substring(4,6));
                         //her opdatere vi vores grafer med x=tid og y=måling
-                        tSeries.getData().add(new XYChart.Data((int)getDateDiff(startDate, new Date(), TimeUnit.SECONDS), Integer.parseInt(fromServer.substring(1,3))));
-                        mSeries.getData().add(new XYChart.Data((int)getDateDiff(startDate, new Date(), TimeUnit.SECONDS), Integer.parseInt(fromServer.substring(4))));
+                        tSeries.getData().add(new XYChart.Data((int)getDateDiff(startDate, new Date(), TimeUnit.SECONDS), Integer.parseInt(decrypted.substring(1,3))));
+                        mSeries.getData().add(new XYChart.Data((int)getDateDiff(startDate, new Date(), TimeUnit.SECONDS), Integer.parseInt(decrypted.substring(4,6))));
                     }
                 });
             }
@@ -95,6 +113,26 @@ public class Main extends Application {
     public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
         long diffInMillies = date2.getTime() - date1.getTime();
         return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+    }
+
+    public static String decrypt(String strToDecrypt)
+    {
+        try
+        {
+            Cipher ci = Cipher.getInstance("AES/CBC/NoPadding");
+            ci.init(Cipher.DECRYPT_MODE, new SecretKeySpec("Just a key123456".getBytes(), "AES"), new IvParameterSpec("Just an IV123456".getBytes()));
+            byte[] b64dced = Base64.getDecoder().decode(strToDecrypt);
+            //husk at beskeden skal være 16 lang.
+            byte[] decd = ci.doFinal(b64dced);
+            String message = new String(decd);
+            System.out.println(message);
+            return message;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void main(String[] args) {
